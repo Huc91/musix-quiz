eb245fa942bacbbcb761d83169c25256689647a1
_Huc_ <luca.ucciero@neprix.com>
2021-10-27 14:50:47 +0200

    feat: track search

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -3,11 +3,14 @@ import React,  { useEffect } from 'react';
 export const Game = () => {
 
     const CORSProxy = window.location.hostname === "localhost" ? "https://cors-anywhere.herokuapp.com/" : "/cors-proxy/";
-    const apiBaseUrl = 'https://api.musixmatch.com/ws/1.1/';
+    const apiBaseUrl = 'https://api.musixmatch.com/ws/1.1/',
+        trackTitle = 'goosebumps',
+        KEY = '71c04f50d5a58a93785ea8ec895155d0',
+        search = `track.search?q_track=${trackTitle}&page_size=10&page=10&s_track_rating=desc&apikey=${KEY}`
 
       const getUser = async () => {
           try {
-              const rawData = await fetch( CORSProxy+apiBaseUrl);
+              const rawData = await fetch( CORSProxy+apiBaseUrl+search);
               const data = await rawData.json();
               console.log(data);
           } catch(err) {
