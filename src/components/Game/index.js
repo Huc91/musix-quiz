7768073f58e5d61587fac3f64ef0ce77fe98a0f3
_Huc_ <luca.ucciero@neprix.com>
2021-10-27 15:12:30 +0200

    fix: lint

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -2,8 +2,8 @@ import React,  { useEffect } from 'react';
 
 export const Game = () => {
 
-    const CORSProxy = window.location.hostname === "localhost" ? "https://cors-anywhere.herokuapp.com/" : "/api/";
-    const apiBaseUrl = 'https://api.musixmatch.com/ws/1.1/',
+    const CORSProxy = window.location.hostname === "localhost" ? "https://cors-anywhere.herokuapp.com/" : "/api/",
+    // const apiBaseUrl = 'https://api.musixmatch.com/ws/1.1/',
         trackTitle = 'goosebumps',
         KEY = '71c04f50d5a58a93785ea8ec895155d0',
         search = `track.search?q_track=${trackTitle}&page_size=10&page=10&s_track_rating=desc&apikey=${KEY}`
