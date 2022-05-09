044348eb13faffdc53c2a5892526a3f9b2140257
_Huc_ <luca.ucciero@neprix.com>
2022-05-09 13:57:14 +0200

    feat: scaffold and state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Redirect } from 'react-router-dom';
+import { useGlobalState } from './state';
 import Routes from './routes';
+import Layout from './views/layout';
 
 import './App.scss';
 
 function App() {
+
+	const [isUserLoggedin] = useGlobalState('isUserLoggedIn');
+
 	return (
 		<div className="App">
-			<BrowserRouter>
-				<Routes />
-			</BrowserRouter>
+			<Layout>
+				<BrowserRouter>
+					<Routes />
+					{isUserLoggedin ? <Redirect to="/"/> : <Redirect to="/login"/>}
+				</BrowserRouter>
+			</Layout>
 		</div>
 	);
 }
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/Home';
-import SetNamePage from './pages/SetName';
-import QuizPage from './pages/Quiz';
-import ResultsPage from './pages/Results';
+import LoginPage from './pages/Login';
+import SignUpPage from './pages/SignUp';
 
 /**
  * Routes component containing routes for the whole application
@@ -11,9 +10,8 @@ import ResultsPage from './pages/Results';
 const Routes = () => (
 	<Switch>
 		<Route exact path="/" component={HomePage} />
-		<Route exact path="/set-name" component={SetNamePage} />
-		<Route exact path="/quiz" component={QuizPage} />
-		<Route exact path="/results" component={ResultsPage} />
+		<Route exact path="/login" component={LoginPage} />
+		<Route exact path="/sign-up" component={SignUpPage} />
 	</Switch>
 );
 
